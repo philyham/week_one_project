4c55849f9847b04e6edbd8ce0defbc806e8701b6
phillybebe <philia.hammond@eggseed.com>
2023-09-03 22:19:36 +0000

    completed task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./index.css"
+import Sedan_Icon from "./images/icon-sedans.svg"
+import Suvs_Icon from "./images/icon-suvs.svg"
+import Luxury_Icon from "./images/icon-luxury.svg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="wrapper">
+     <div className="sedans">
+       <img src={Sedan_Icon} alt="sedan icon" />
+       <h2>SEDANS</h2>
+       <p>
+        Choose a sedan for its affordability and excellent fuel economy. 
+        Ideal for cruising in the city and or on your next road trip.
+       </p>
+       <button type="button" className="sedan-button">Learn More</button>
+     </div>
+
+     <div className="suvs">
+       <img src={Suvs_Icon} alt="suvs icon" />
+       <h2>SUVS</h2>
+       <p>
+        Take an SUV for its spacious interior, power and versability.
+        Perfect for your next family vacation and off-road adventures.
+       </p>
+       <button type="button" className="suvs-button">Learn More</button>
+     </div>
+
+     <div className="luxury">
+       <img src={Luxury_Icon} alt="luxury icon" />
+       <h2>LUXURY</h2>
+       <p>
+        Cruise in the best car brands without the bloated prices. 
+        Enjoy the enhanced comfort of a luxury rental and arrive in style.
+       </p>
+       <button type="button" className="luxury-button">Learn More</button>
+     </div>
+    </div>
     </div>
   );
 }
